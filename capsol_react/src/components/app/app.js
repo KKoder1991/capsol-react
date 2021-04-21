95b7fc3088bcdc0eb37a2e387d2125d5ed86e774
KKoder1991 <konondlovu@icloud.com>
2021-04-21 11:16:16 +0200

    Delete boilerplate code, replace with CapSol

--- capsol_react/src/components/app/app.js
+++ capsol_react/src/components/app/app.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './components/app/App.css';
+import React from 'react';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/app/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+      return (
+            <div className="App">
+                <div className="Body">
+                    <h1>CapSol Application</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
