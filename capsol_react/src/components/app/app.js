63720bc37d8615ae1780e6945604aedc922edea1
KKoder1991 <konondlovu@icloud.com>
2021-04-23 11:52:17 +0200

    Made a class a function and wrapped comp in divs

--- capsol_react/src/components/app/app.js
+++ capsol_react/src/components/app/app.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import './app.css';
+import logo from '../app/capsol-logo.png';
+import TextInput from '../TextInput/TextInput';
+import UploadImage from '../UploadImage/UploadImage';
+import TogglePrivateOrPublic from '../TogglePrivateOrPublic/TogglePrivateOrPublic';
 
-class App extends React.Component {
-  render(){
+function App() {
+      const [selected, setSelected] = useState(false);
       return (
             <div className="App">
-                <div className="Body">
+                <div className="capsol-heading">
                     <h1>CapSol Application</h1>
+                </div>
+                <div className="logo">
+                    <img src={logo} alt="logo for capsol"></img>
+                </div>
+                <div className="TextInput">
+                    <TextInput />
+                </div>
+                <div className="UploadImage">
+                    <UploadImage id='UploadImage' />
+                </div>
+                <div className="TogglePrivateOrPublic">
+                    <TogglePrivateOrPublic 
+                        selected={selected}
+                        toggleSelected={() => {
+                            setSelected (!selected);
+                        }}
+                    />
                 </div>
             </div>
         );
     }
-}
 
 export default App;
