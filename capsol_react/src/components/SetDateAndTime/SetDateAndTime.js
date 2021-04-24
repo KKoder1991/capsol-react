173c0940af237cd9450584c6bfa668a8f4dcf29c
KKoder1991 <konondlovu@icloud.com>
2021-04-24 16:41:19 +0200

    Create component to set time and date

---
+++ capsol_react/src/components/SetDateAndTime/SetDateAndTime.js
@@ -0,0 +1,16 @@
+import './SetDateAndTime.css';
+
+import React from 'react';
+
+export default class SetDateAndTime extends React.Component{
+    render(){
+        return(
+            <div className="SetDateAndTime">
+                  <p>Date & Time</p>
+                  <button></button>
+            </div>
+          
+
+        )
+    }
+}
