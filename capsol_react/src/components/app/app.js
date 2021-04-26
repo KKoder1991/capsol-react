97d232e2230316976dc06d48ab9464a78b83458b
KKoder1991 <konondlovu@icloud.com>
2021-04-26 10:54:19 +0200

    Refactor code, Add Duration comp

--- capsol_react/src/components/app/app.js
+++ capsol_react/src/components/app/app.js
@@ -4,33 +4,36 @@ import logo from '../app/capsol-logo.png';
 import TextInput from '../TextInput/TextInput';
 import UploadImage from '../UploadImage/UploadImage';
 import TogglePrivateOrPublic from '../TogglePrivateOrPublic/TogglePrivateOrPublic';
+import ParagraphInput from '../ParagraphInput/ParagraphInput';
+import SetDateAndTime from '../SetDateAndTime/SetDateAndTime';
+import Duration from '../Duration/Duration';
 
-function App() {
-      const [selected, setSelected] = useState(false);
-      return (
+export default class App extends React.Component {
+      render() {
+          return (
             <div className="App">
-                <div className="capsol-heading">
-                    <h1>CapSol Application</h1>
-                </div>
                 <div className="logo">
                     <img src={logo} alt="logo for capsol"></img>
                 </div>
                 <div className="TextInput">
                     <TextInput />
                 </div>
+                <div className="Duration">
+                    <Duration />
+                </div>
+                <div className="SetDateAndTime">
+                    <SetDateAndTime />
+                </div>
                 <div className="UploadImage">
                     <UploadImage id='UploadImage' />
                 </div>
                 <div className="TogglePrivateOrPublic">
-                    <TogglePrivateOrPublic 
-                        selected={selected}
-                        toggleSelected={() => {
-                            setSelected (!selected);
-                        }}
-                    />
+                    <TogglePrivateOrPublic text="Private"  />
+                </div>
+                <div className="ParagraphInput">
+                    <ParagraphInput />
                 </div>
             </div>
-        );
+        )
     }
-
-export default App;
+}
