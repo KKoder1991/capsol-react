ad63211f605499e00d3077677dc1f3517080f2c7
KKoder1991 <konondlovu@icloud.com>
2021-05-03 16:32:23 +0200

    Add text area placeholder

--- capsol_react/src/components/ParagraphInput/ParagraphInput.js
+++ capsol_react/src/components/ParagraphInput/ParagraphInput.js
@@ -6,7 +6,7 @@ export default class ParagraphInput extends React.Component{
     render(){
         return(
             <div className="ParagraphInput">
-                <textarea id="ParagraphInput" placeholder="Say something about your pop up">
+                <textarea  placeholder="Say something about your pop up">
             </textarea>
         </div>
         );
