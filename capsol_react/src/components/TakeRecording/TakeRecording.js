6fdc34ee1722215f76ce23d7a4ad20cb973c6eda
KKoder1991 <konondlovu@icloud.com>
2021-05-03 16:34:46 +0200

    Create dummy to render in app of component

---
+++ capsol_react/src/components/TakeRecording/TakeRecording.js
@@ -0,0 +1,11 @@
+import '../TakeRecording/TakeRecording.css';
+
+import React from 'react';
+
+export default class TakeRecording extends React.Component{
+    render(){
+        return(
+            <button>Take Recording</button>
+        )
+    }
+}
