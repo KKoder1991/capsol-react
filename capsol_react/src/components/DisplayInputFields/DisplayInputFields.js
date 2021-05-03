bb956806050aeaac513dcc695104ebfdc4135b33
KKoder1991 <konondlovu@icloud.com>
2021-05-03 17:22:15 +0200

    Add upload image and buttons for form submission

--- capsol_react/src/components/DisplayInputFields/DisplayInputFields.js
+++ capsol_react/src/components/DisplayInputFields/DisplayInputFields.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './DisplayInputFields.css';
+import SetDateAndTime from '../SetDateAndTime/SetDateAndTime.js';
+import TextInput from '../TextInput/TextInput.js';
+import UploadImage from '../UploadImage/UploadImage';
+import SaveButtonForm from '../SaveButtonForm/SaveButtonForm.js';
+import CancelButtonForm from '../CancelButtonForm/CancelButtonForm';
 
 export default class DisplayInputFields extends React.Component{
     render(){
@@ -16,10 +21,20 @@ export default class DisplayInputFields extends React.Component{
                 <label>Where?</label>
                 <TextInput />
             </div>
-            {/* Insert add a photo component here */}
+            <div className="UploadImage">
+                <UploadImage />
+            </div>
             <div className="TellUsMore">
                 <label>Tell Us More</label>
                 <TextInput />
+            </div>
+            <div className="FormButtons">
+            <div className="SaveButtonForm">
+                <SaveButtonForm />
+            </div>
+            <div className="CancelButtonForm">
+                <CancelButtonForm />
+            </div>
             </div>
           </div> 
         );
