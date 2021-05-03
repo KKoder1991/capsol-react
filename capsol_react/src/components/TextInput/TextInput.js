4714f53ec5741340dae2e467303eeef3a83bc06c
KKoder1991 <konondlovu@icloud.com>
2021-05-03 16:33:28 +0200

    Add div to wrap text input

--- capsol_react/src/components/TextInput/TextInput.js
+++ capsol_react/src/components/TextInput/TextInput.js
@@ -4,10 +4,12 @@ import './TextInput.css';
 class TextInput extends React.Component {
     render(){
         return(
-            <input 
-                id="TextInput"
-                placeholder="What's popping up?"
-            />
+            <div className="ClassTextInput">
+                <input 
+                    id="TextInput"
+                    placeholder="What's popping up?"
+                 />
+            </div>
         );
     }
 }
