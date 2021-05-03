1e1e2990beced66132a7919977da036e4aaa0251
KKoder1991 <konondlovu@icloud.com>
2021-05-03 17:20:55 +0200

    Create buttons for form submission or cancel

---
+++ capsol_react/src/components/CancelButtonForm/CancelButtonForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CancelButtonForm.css';
+
+export default class CancelButtonForm extends React.Component{
+    render(){
+        return(
+            <button>Cancel</button>
+        );
+    };
+}
---
+++ capsol_react/src/components/SaveButtonForm/SaveButtonForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SaveButtonForm.css';
+
+export default class SaveButtonForm extends React.Component{
+    render(){
+        return(
+            <button>Save</button>
+        );
+    };
+}
