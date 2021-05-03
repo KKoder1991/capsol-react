4c202bc9abb3565bd1e813a56c24fba2e28af435
KKoder1991 <konondlovu@icloud.com>
2021-05-03 16:31:22 +0200

    Remove components rendered in app, to be reworked

--- capsol_react/src/components/app/app.js
+++ capsol_react/src/components/app/app.js
@@ -1,39 +1,16 @@
-import React from 'react';
-import './app.css';
-import logo from '../app/capsol-logo.png';
-import TextInput from '../TextInput/TextInput';
-import UploadImage from '../UploadImage/UploadImage';
-import TogglePrivateOrPublic from '../TogglePrivateOrPublic/TogglePrivateOrPublic';
-import ParagraphInput from '../ParagraphInput/ParagraphInput';
-import SetDateAndTime from '../SetDateAndTime/SetDateAndTime';
-import Duration from '../Duration/Duration';
+import React from "react";
+import "./app.css";
+import logo from "../app/capsol-logo.png";
 
 export default class App extends React.Component {
-      render() {
-          return (
-            <div className="App">
-                <div className="logo">
-                    <img src={logo} alt="logo for capsol"></img>
-                </div>
-                <div className="TextInput">
-                    <TextInput />
-                </div>
-                <div className="Duration">
-                    <Duration />
-                </div>
-                <div className="SetDateAndTime">
-                    <SetDateAndTime />
-                </div>
-                <div className="UploadImage">
-                    <UploadImage id='UploadImage' />
-                </div>
-                <div className="TogglePrivateOrPublic">
-                    <TogglePrivateOrPublic text="Private"  />
-                </div>
-                <div className="ParagraphInput">
-                    <ParagraphInput />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="CapSolLogo">
+          <img src={logo} alt="logo for capsol"></img>
+        </div>
+
+      </div>
+    );
+  }
 }
