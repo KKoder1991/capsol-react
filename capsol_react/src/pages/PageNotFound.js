ca06af761605c00c9da9623c6ba3970c629e88f2
KKoder1991 <konondlovu@icloud.com>
2021-05-04 15:11:11 +0200

    Create page for when routing to non-existing page

---
+++ capsol_react/src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const PageNotFound = () => {
+    return(
+            <div>
+                <h3>Page not found</h3>
+                <small>Main Page</small>
+            </div>
+    );
+};
+
+export default PageNotFound;
