92f8528d61afdbe444a6b182576a169206b923a2
KKoder1991 <konondlovu@icloud.com>
2021-05-04 15:11:51 +0200

    Change import statement to reflect new file str

--- capsol_react/src/index.js
+++ capsol_react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/app.js';
+import App from '../src/app/app.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
