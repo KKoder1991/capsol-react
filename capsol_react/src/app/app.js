6bd2e24d6fd8b51ef4e62fc4180144df096210ac
KKoder1991 <konondlovu@icloud.com>
2021-05-04 15:12:51 +0200

    add react router func, constructor for state

--- capsol_react/src/app/app.js
+++ capsol_react/src/app/app.js
@@ -1,21 +1,42 @@
 import React from "react";
 import "./app.css";
 import logo from "./capsol-logo.png";
-import SetDateAndTime from "../components/SetDateAndTime/SetDateAndTime";
-import TextInput from "../components/TextInput/TextInput";
+
+//Pages to be rendered in the router
+import DisplayPopUps from "../pages/DisplayPopUps/DisplayPopUps";
 import DisplayInputFields from "../pages/DisplayInputFields/DisplayInputFields";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentPage: false,
+      setPage: 3
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="CapSolLogo">
           <img src={logo} alt="logo for capsol"></img>
         </div>
-            <div className="DisplayInputField">
-                <DisplayInputFields />
-            </div>
-
+        <div className="DisplayPages">
+          <Router>
+            <Switch>
+              <Redirect to="/404"/>
+              <Route exact path="/" component={DisplayInputFields} />
+              <Route component={DisplayPopUps} />
+            </Switch>
+           </Router>
+        </div>
       </div>
     );
   }
