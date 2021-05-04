a4d95b52b3467899d5eccd0985cfda5ce7925dde
KKoder1991 <konondlovu@icloud.com>
2021-05-04 12:15:22 +0200

    Create cancel button for cancelling the add info

--- capsol_react/src/components/CancelButtonForm/CancelButtonForm.js
+++ capsol_react/src/components/CancelButtonForm/CancelButtonForm.js
@@ -4,7 +4,7 @@ import './CancelButtonForm.css';
 export default class CancelButtonForm extends React.Component{
     render(){
         return(
-            <button>Cancel</button>
+            <button id="CancelButton">Cancel</button>
         );
     };
 }
