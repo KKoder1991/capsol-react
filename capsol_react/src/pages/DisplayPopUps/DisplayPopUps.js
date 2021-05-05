d7842226f2a53f8310ee73f3e489a799d336ce20
KKoder1991 <konondlovu@icloud.com>
2021-05-05 14:00:53 +0200

    Add styling for DisplayPopUps and dummy map cont

--- capsol_react/src/pages/DisplayPopUps/DisplayPopUps.js
+++ capsol_react/src/pages/DisplayPopUps/DisplayPopUps.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './DisplayPopUps.css';
+import React from "react";
+import "./DisplayPopUps.css";
 
-export default class DisplayPopUps extends React.Component{
-    render(){
-        return(
-            <div class="grid-container"></div>
-        );
-    };
+export default class DisplayPopUps extends React.Component {
+  render() {
+    return (
+      <div class="grid-container">
+        <div className="container-1">
+            <header></header>
+        </div>
+        <div className="container-1">
+            <ul>
+                {["420 pop up stream", "Guitar Session/ Mark Makado", "Peruvian Music by Kerridy ensemble", "Lee Borthers and friends playing jazz"]
+                .map((events, idx) => { return <li key={idx}>{events}</li>})}
+            </ul>
+        </div>
+      </div>
+    );
+  }
 }
