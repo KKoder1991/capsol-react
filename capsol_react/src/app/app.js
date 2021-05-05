362bee08bf1dab11694ab6ad618f49e65fd4a5b3
KKoder1991 <konondlovu@icloud.com>
2021-05-05 14:01:35 +0200

    Add routes for different pages, refactor some code

--- capsol_react/src/app/app.js
+++ capsol_react/src/app/app.js
@@ -3,10 +3,11 @@ import "./app.css";
 import logo from "./capsol-logo.png";
 
 //Pages to be rendered in the router
+import Homepage from "../pages/Homepage/Homepage";
 import DisplayPopUps from "../pages/DisplayPopUps/DisplayPopUps";
 import DisplayInputFields from "../pages/DisplayInputFields/DisplayInputFields";
 //Page to be rendered when routing to a non-existent page
-import PageNotFound from '../pages/PageNotFound';
+import PageNotFound from "../pages/PageNotFound";
 
 import {
   BrowserRouter as Router,
@@ -16,14 +17,16 @@ import {
   Redirect,
 } from "react-router-dom";
 
-export default class App extends React.Component {
-  constructor(props){
+export default  class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       currentPage: false,
-      setPage: 3
-    }
-  }
+      setPage: 3,
+    };
+  };
+
+
   render() {
     return (
       <div className="App">
@@ -32,27 +35,29 @@ export default class App extends React.Component {
         </div>
         <div className="DisplayPages">
           <Router>
-          <div>
-        <ul>
-          <li>
-            <Link to="/">Pop Ups</Link>
-          </li>
-          <li>
-            <Link to="/DisplayInputFields">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
+            <div>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/PopUps">Pop Ups</Link>
+                </li>
+                <li>
+                  <Link to="/DisplayInputFields">Form</Link>
+                </li>
+              </ul>
+            </div>
 
-        <hr />
+            <hr />
             <Switch>
-              <Route exact path="/" component={DisplayPopUps} />
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/PopUps" component={DisplayPopUps} />
               <Route exact path="/DisplayInputFields" component={DisplayInputFields} />
               <Route exact path="/PageNotFound" component={PageNotFound} />
-              <Redirect to="/PageNotFound"/>
+              <Redirect to="/PageNotFound" />
             </Switch>
-           </Router>
+          </Router>
         </div>
       </div>
     );
