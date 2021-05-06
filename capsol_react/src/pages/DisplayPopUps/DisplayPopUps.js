71d77ad0f722b41ebe1f41ccf0da667f9dca6cfc
KKoder1991 <konondlovu@icloud.com>
2021-05-06 15:09:24 +0200

    write function to get data saved to DB

--- capsol_react/src/pages/DisplayPopUps/DisplayPopUps.js
+++ capsol_react/src/pages/DisplayPopUps/DisplayPopUps.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./DisplayPopUps.css";
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function DisplayPopUps (){
+
+  const [ eventList, setEventList ] = useState([]);
+
+  const getEvents = () => {
+    axios.get('http://localhost:3000/events').then((response) => {
+      console.log(response);
+    });
+  };
     return (
       <div class="grid-container">
         <div className="container-1">
             <header></header>
         </div>
+        <button onClick={getEvents} >Events</button>
         <div className="container-1">
             <ul>
                 {["420 pop up stream", "Guitar Session/ Mark Makado", "Peruvian Music by Kerridy ensemble", "Lee Borthers and friends playing jazz"]
