a7c21449361ca16565d90a95636b29b7e8177add
KKoder1991 <konondlovu@icloud.com>
2021-05-06 14:21:53 +0200

    Uncomment onClick field and add function to it

--- capsol_react/src/components/SaveButtonForm/SaveButtonForm.js
+++ capsol_react/src/components/SaveButtonForm/SaveButtonForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './SaveButtonForm.css';
+import addEvent from 
 
 export default class SaveButtonForm extends React.Component{
     render(){
         return(
-            <button id="SaveButton" /*onClick={}*/ >Save</button>
+            <button id="SaveButton" onClick={addEvent} >Save</button>
         );
     };
 }
