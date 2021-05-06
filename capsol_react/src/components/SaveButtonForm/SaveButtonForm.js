fb6c36a0b61121710765e4d1e248842e139cbe76
KKoder1991 <konondlovu@icloud.com>
2021-05-06 14:32:28 +0200

    Take off onclick event and replace class with but

--- capsol_react/src/components/SaveButtonForm/SaveButtonForm.js
+++ capsol_react/src/components/SaveButtonForm/SaveButtonForm.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './SaveButtonForm.css';
-import addEvent from 
 
 export default class SaveButtonForm extends React.Component{
     render(){
         return(
-            <button id="SaveButton" onClick={addEvent} >Save</button>
+            <button id="SaveButton"  >Save</button>
         );
     };
 }
