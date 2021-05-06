dec5cb5ed28e7a2f05b50e6548e913adc67f2c04
KKoder1991 <konondlovu@icloud.com>
2021-05-06 10:44:01 +0200

    Add database information to create DB connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express()
 const mysql = require("mysql");
 
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'NB2020_Apple',
+    database: 'CapSol-System'
+});
+
 app.listen(3001, () => {
     console.log("Your server is running on 3001");
 });
