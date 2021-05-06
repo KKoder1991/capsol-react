0636240a26b2b827656412fc460f4af023c2c413
KKoder1991 <konondlovu@icloud.com>
2021-05-06 11:05:16 +0200

    complete db query code block, format document

--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'NB2020_Apple',
-    database: 'CapSol-System'
+  user: "root",
+  host: "localhost",
+  password: "NB2020_Apple",
+  database: "CapSol-System",
 });
 
-app.post('/create', (req, res) => {
-    const eventName = req.body.eventName;
-    const eventDate = req.body.eventDate;
-    const eventLocation = req.body.eventLocation;
-    const eventImage = req.body.eventImage;
-    const eventDescription = req.body.eventDescription;
-})
+app.post("/create", (req, res) => {
+  const eventName = req.body.eventName;
+  const eventDate = req.body.eventDate;
+  const eventLocation = req.body.eventLocation;
+  const eventImage = req.body.eventImage;
+  const eventDescription = req.body.eventDescription;
+
+  db.query(
+    "INSERT INTO CapSol-Events (eventName, eventDate, eventLocation, eventImage, eventDescription) VALUES (?, ?, ?, ?, ?)",
+    [eventName, eventDate, eventLocation, eventImage, eventDescription],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values inserted");
+      }
+    }
+  );
+});
 
 app.listen(3001, () => {
-    console.log("Your server is running on 3001");
+  console.log("Your server is running on 3001");
 });
