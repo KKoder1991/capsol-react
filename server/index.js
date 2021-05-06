79db965415141386a615968b9534c123e9324904
KKoder1991 <konondlovu@icloud.com>
2021-05-06 10:53:54 +0200

    New consts for post info, change name struct

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ const db = mysql.createConnection({
     database: 'CapSol-System'
 });
 
+app.post('/create', (req, res) => {
+    const eventName = req.body.eventName
+    const eventDate = req.body.eventDate
+    const eventLocation = req.body.eventLocation
+    const eventImage = req.body.eventImage
+    const eventDescription = req.body.eventDescription
+})
+
 app.listen(3001, () => {
     console.log("Your server is running on 3001");
 });
