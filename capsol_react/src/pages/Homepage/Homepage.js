080d3956df40341b2df0ca811963b05e9b0ed13a
KKoder1991 <konondlovu@icloud.com>
2021-05-09 09:35:53 +0200

    Add content to homepage

--- capsol_react/src/pages/Homepage/Homepage.js
+++ capsol_react/src/pages/Homepage/Homepage.js
@@ -6,7 +6,7 @@ export default class Homepage extends React.Component {
     return (
       <div class="grid-container">
         <div className="container-1">
-            <header>Welcome to CapSol</header>
+            <h1>Welcome to CapSol</h1>
         </div>
         <div className="About Us">
             <p>
@@ -14,6 +14,17 @@ export default class Homepage extends React.Component {
                 scroll through or add your pop up event to let others know about your
                 event!
             </p>
+        </div>
+        <div className="bullet-points">
+          <ul>
+            <li>Claim a pop-up spot</li>
+
+            <li>Tell people about yout pop-up in a digital CapSol</li>
+
+            <li>Drop your CapSol on a map</li>
+            
+            <li>Let others discover, join and interact with your CapSol</li>
+          </ul>
         </div>
       </div>
     );
